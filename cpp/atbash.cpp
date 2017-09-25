47c4614509aab279a9e53eec32ae545739d4ea70
dospunk <lostboycmd@gmail.com>
2017-09-25 19:21:25 -0400

    Add code comments

--- cpp/atbash.cpp
+++ cpp/atbash.cpp
@@ -6,11 +6,11 @@ string atbash(string str){
 	string output;
 	string::iterator iter;
 	for(iter = str.begin(); iter < str.end(); iter++){
-		if ((*iter >= 'A') && (*iter <= 'Z')) {
+		if ((*iter >= 'A') && (*iter <= 'Z')) { //if character is capital
 			output += char(('A'+'Z')-*iter);
-		} else if ((*iter >= 'a') && (*iter <= 'z')) {
+		} else if ((*iter >= 'a') && (*iter <= 'z')) { //if character is lowercase
 			output += char(('a'+'z')-*iter);
-		} else {
+		} else { //if character isn't a letter
 			output += char(*iter);
 		}
 	}
