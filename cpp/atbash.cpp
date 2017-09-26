0aa4420bf32927c4311257248b95b2a028708669
dospunk <lostboycmd@gmail.com>
2017-09-26 01:00:07 -0400

    fix i++ -> ++i

--- cpp/atbash.cpp
+++ cpp/atbash.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string atbash(string str){
 	string output;
 	string::iterator iter;
-	for(iter = str.begin(); iter < str.end(); iter++){
+	for(iter = str.begin(); iter < str.end(); ++iter){
 		if ((*iter >= 'A') && (*iter <= 'Z')) { //if character is capital
 			output += char(('A'+'Z')-*iter);
 		} else if ((*iter >= 'a') && (*iter <= 'z')) { //if character is lowercase
--- cpp/rot.cpp
+++ cpp/rot.cpp
@@ -5,7 +5,7 @@ using namespace std;
 string rot(string str, int num=13){
 	string output;
 	string::iterator iter;
-	for(iter = str.begin(); iter < str.end(); iter++){
+	for(iter = str.begin(); iter < str.end(); ++iter){
 		if(((*iter <= ('Z'-num)) && (*iter >= 'A')) || ((*iter <= ('z'-num)) && (*iter >= 'a'))){
 			output += char(*iter+num);
 		} else if(((*iter >= ('Z'-num)) && (*iter <= 'z')) || ((*iter >= ('z'-num)) && (*iter <= 'z'))){
