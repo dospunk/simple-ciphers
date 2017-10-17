7a71e56502a89a8d2ac8486552451fda9c2e662e
dospunk <lostboycmd@gmail.com>
2017-10-17 18:16:30 -0400

    reorganize and improve c++ test file

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -9,26 +9,48 @@
 using namespace std;
 
 int main() {
-	//utils
-	assert(!bool(filter("abcdefghijk", "abcefhij").compare("dgk")));
-	cout << "Filter: abcdefghijk - abcefhij = " << filter("abcdefghijk", "abcefhij") << endl;
-	assert(!bool(removeDuplicates("aabbcccd567ij").compare("abcd567ij")));
-	cout << "Remove Duplicates: aabbcccd567ij -> " << removeDuplicates("aabbcccd567ij") << endl;
-	assert(!bool(lowerString("AbCd5Y+uU").compare("abcd5y+uu")));
-	cout << "Lower String: AbCd5Y+uU -> " << lowerString("AbCd5Y+uU") << endl;
-	cout << endl;
-	// ciphers
-	assert(!bool(atbash("He<l0l|o").compare("Sv<o0o|l")));
-	cout << "Atbash: He<l0l|o -> " << atbash("He<l0l|o") << endl;
-	assert(!bool(rot("[Hello", 1).compare("[Ifmmp")));
-	cout << "Rot1: [Hello -> " << rot("[Hello", 1) << endl;
-	assert(!bool(rot("Hel.lo", 7).compare("Ols.sv")));
-	cout << "Rot7: Hel.lo -> " << rot("Hel.lo", 7) << endl;
-	assert(!bool(rot("H-e-l-l-o").compare("U-r-y-y-b")));
-	cout << "Rot13: H-e-l-l-o -> " << rot("H-e-l-l-o") << endl;
-	assert(!bool(morse("Whoa .-/").compare(".-- .... --- .- / period dash forwardslash ")));
-	cout << "Morse Code: 'Whoa .-/' -> " << morse("Whoa .-/") << endl;
-	assert(!bool(keyword("s7eeeeeecret", "bestKey").compare("p7kkkkkksokq")));
-	cout << "Keyword Cipher: 's7eeeeeecret' with keyword 'bestKey' -> " << keyword("s7eeeeeecret", "bestkey") << endl;
+	{//tests utils
+		assert(filter("abcdefghijk", "abcefhij") == "dgk");
+		assert(removeDuplicates("aabbcccd567ij") == "abcd567ij");
+		assert(lowerString("AbCd5Y+uU") == "abcd5y+uu");
+		#ifdef NDEBUG
+			cout << "Filter: abcdefghijk - abcefhij = " << filter("abcdefghijk", "abcefhij") << endl;
+			cout << "Remove Duplicates: aabbcccd567ij -> " << removeDuplicates("aabbcccd567ij") << endl;
+			cout << "Lower String: AbCd5Y+uU -> " << lowerString("AbCd5Y+uU") << endl << endl;
+		#endif
+	}
+	{//test atbash
+		assert(atbash("He<l0l|o") == "Sv<o0o|l");
+		#ifdef NDEBUG
+			cout << "Atbash: He<l0l|o -> " << atbash("He<l0l|o") << endl;
+		#endif
+	}
+	{//test rot-n
+		assert(rot("[Hello", 1) == "[Ifmmp");
+		assert(rot("Hel.lo", 7) == "Ols.sv");
+		assert(rot("H-e-l-l-o") == "U-r-y-y-b");
+		#ifdef NDEBUG
+			cout << "Rot1: [Hello -> " << rot("[Hello", 1) << endl;
+			cout << "Rot7: Hel.lo -> " << rot("Hel.lo", 7) << endl;
+			cout << "Rot13: H-e-l-l-o -> " << rot("H-e-l-l-o") << endl;
+		#endif
+	}
+	{//test morse
+		assert(morse("Whoa .-/") == ".-- .... --- .- / period dash forwardslash ");
+		#ifdef NDEBUG
+			cout << "Morse Code: 'Whoa .-/' -> " << morse("Whoa .-/") << endl;
+		#endif
+	}
+	{//test keyword
+		assert(keyword("s7eeeeeecret", "bestKey") == "p7kkkkkksokq");
+		#ifdef NDEBUG
+			cout << "Keyword Cipher: 's7eeeeeecret' with keyword 'bestKey' -> "
+			     << keyword("s7eeeeeecret", "bestkey") << endl;
+		#endif
+	}
+
+	#ifndef NDEBUG
+	cout << "All tests passed!" << endl;
+	#endif
 	return 0;
 }
