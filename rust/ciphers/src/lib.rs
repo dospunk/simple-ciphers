583d3eb06429c2d1bdd923b98c202f66dfdcc099
dospunk <lostboycmd@gmail.com>
2017-10-30 15:44:00 -0400

    Add rot decode fn to rust make magic numbers into consts

--- rust/ciphers/src/lib.rs
+++ rust/ciphers/src/lib.rs
@@ -6,5 +6,7 @@ mod tests {
     #[test]
     fn unit_test_rot() {
         assert_eq!(String::from("uryyb"), rot::encode("hello", 13));
+        assert_eq!(String::from("hello"), rot::decode("uryyb", 13));
+        assert_eq!(String::from("I like food"), rot::decode("M pmoi jssh", 4));
     }
 }
--- rust/ciphers/src/rot/mod.rs
+++ rust/ciphers/src/rot/mod.rs
@@ -1,15 +1,39 @@
+#![allow(non_upper_case_globals)]
+
+const A_val: u8 = 65;
+const a_val: u8 = 97;
+const Z_val: u8 = 90;
+const z_val: u8 = 122;
+
 pub fn encode(s: &str, n: u8) -> String{
     let mut output = String::new();
     for c in s.chars(){
         let x = c as u8;
-        let letter_before_n = x <= 90-n && x >= 65 || x <= 122-n && x >= 97;
-        let letter_after_n = x >= 90-n && x <= 90 || x >= 122-n && x <= 122;
+        let letter_before_n = x <= Z_val-n && x >= A_val || x <= z_val-n && x >= a_val;
+        let letter_after_n = x >= Z_val-n && x <= Z_val || x >= z_val-n && x <= z_val;
         if letter_before_n {
             output.push(char::from(x+n));
         } else if letter_after_n {
             output.push(char::from(x-26+n));
         } else {
-            output.push(char::from(n));
+            output.push(c);
+        }
+    }
+    output //return
+}
+
+pub fn decode(s: &str, n: u8) -> String{
+    let mut output = String::new();
+    for c in s.chars(){
+        let x = c as u8;
+        let letter_after_n = x >= A_val+n && x <= Z_val || x >= a_val+n && x <= z_val;
+        let letter_before_n = x < A_val+n && x >= A_val || x < a_val+n && x >= a_val;
+        if letter_after_n {
+            output.push(char::from(x-n));
+        } else if letter_before_n {
+            output.push(char::from(x+26-n));
+        } else {
+            output.push(c);
         }
     }
     output //return
