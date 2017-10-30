457409fcc845336ab7f82a477aabd10f61481735
dospunk <lostboycmd@gmail.com>
2017-10-30 16:02:14 -0400

    add rot integration tests rust

--- rust/ciphers/tests/integration_test.rs
+++ rust/ciphers/tests/integration_test.rs
@@ -3,4 +3,6 @@ extern crate ciphers;
 #[test]
 fn int_test_rot(){
     assert_eq!(String::from("uryyb"), ciphers::rot::encode("hello", 13));
+    assert_eq!(String::from("hello"), ciphers::rot::decode("uryyb", 13));
+    assert_eq!(String::from("I like food"), ciphers::rot::decode("M pmoi jssh", 4));
 }
